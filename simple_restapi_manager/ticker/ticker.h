550adecb8197f59c67427fae32adea7e648c882b
root <root@smartrich.org>
2013-08-21 17:41:40 +0800

    add ticker.h

---
+++ simple_restapi_manager/ticker/ticker.h
@@ -0,0 +1,23 @@
+#ifndef TICKER_H
+#define TICKER_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+const char*
+get_account_info(const char*, const char*, const char*, const char*);
+const char*
+get_position_info(const char*, const char*, const char*, const char*);
+const char*
+order_insert(const char* instrument, const char* price,const char *director, const char *offset, const char *volume
+	, const char* f, const char *b, const char *u, const char *p);
+const char*
+order_delete(const char *instrument, const char *session, const char *frontid, const char* orderref
+	, const char *f, const char *b, const char *u, const char *p);
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif // TICKER_H
